71ad6634cef25ad4eafa6cd6de12eebb7b96dbc2
surajith <surajithreddy@gmail.com>
2018-12-01 16:53:50 +0530

    Adding css file for ImageLsit

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,10 +1,11 @@
+import "./ImageList";
 import React from "react";
 
 const ImageList=(props)=>{
    const Imgs= props.Imageslist.map((image)=>{
     return (
         <figure key={image.id}>        
-           <abbr title={image.description}><img  src={image.urls.regular} alt={image.description} width="200px" height="200px"/></abbr> 
+           <abbr title={image.description}><img  src={image.urls.regular} alt={image.description} width="800px" height="500px"/></abbr> 
             <p>Posted By {image.user.instagram_username}</p>
         </figure>
        )
--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 
+class Search extends React.Component{
+    render(){
+        return(
+            //Adding css directly using style
+            <div className="ui container" style={{marginTop: "20px"}}>
+                <App/>
+            </div>
+        )
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Search />, document.getElementById('root'));
 
 
